3a82743704be45cca86c631bf413158e92298f45
Magnus Olofsson <magnusolofsson@outlook.com>
2024-12-02 09:10:45 +0100

    Preparing for  step 2

--- 02/Program.cs
+++ 02/Program.cs
@@ -2,10 +2,12 @@
 
 
 int validDataCount = 0;
+int step2ValidCount = 0;
 foreach(var entry in uData)
 {    
     validDataCount += entry.IsValid ? 1 : 0;
 }
 
 Console.WriteLine($"Step 1: {validDataCount}");
+Console.WriteLine($"Step 2: {step2ValidCount}");
 
--- 02/model/unusualData.cs
+++ 02/model/unusualData.cs
@@ -39,8 +39,7 @@ public class unusualData
         return true;
     }
 
-
-
     
+
     
 }
