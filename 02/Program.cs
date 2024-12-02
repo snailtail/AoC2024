ae4238b57a863788599bcfca2c1e5508ce5f97cc
Magnus Olofsson <magnusolofsson@outlook.com>
2024-12-02 08:35:35 +0100

    Solved day 2 part 1

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,28 +1,11 @@
-﻿var unusualData = File.ReadLines("../data/02test.dat").ToArray().Select(l => l.Split(' ').Select(i => int.Parse(i)).ToArray()).ToArray();
+﻿unusualData[] uData = File.ReadLines("02.dat").ToArray().Select(l => l.Split(' ').Select(i => int.Parse(i)).ToArray()).Select(arr => new unusualData(arr)).ToArray();
 
 
-
-foreach(var entry in unusualData)
-{
-    //check "direction"
-    bool isIncreasing = false;
-    isIncreasing = entry[1] > entry[0];
-    var result = isValidStep1(entry,isIncreasing);
+int validDataCount = 0;
+foreach(var entry in uData)
+{    
+    validDataCount += entry.IsValid ? 1 : 0;
 }
 
-
-
-bool isValidStep1(int[] arr, bool isIncreasing)
-{
-    
-    for(int i = 0; i < arr.Length-1; i++)
-    {
-        if((isIncreasing && arr[i+1] < arr[i]) || (!isIncreasing && arr[i+1] > arr[i]))
-        {
-            // 
-            return false;
-        }
-    }
-}
-Console.WriteLine("");
+Console.WriteLine($"Step 1: {validDataCount}");
 
---
+++ 02/model/unusualData.cs
@@ -0,0 +1,46 @@
+public class unusualData
+{
+    private int[] _data;
+    public int[] Data
+    {
+        get { return _data; }
+        set { _data = value; }
+    }
+
+    public unusualData(int[] data)
+    {
+        _data = data;
+    }
+
+    public bool IsIncreasing => _data[1] > _data[0];
+
+    public bool IsValid => isValid();
+
+    private bool isValid()
+    {    
+        for(int i = 0; i < _data.Length-1; i++)
+        {
+
+            // Check if increasing or decreasing breaks
+            if((this.IsIncreasing && _data[i+1] < _data[i]) || (!this.IsIncreasing && _data[i+1] > _data[i]))
+            {
+                // 
+                return false;
+            }
+
+            // Check if gaps between elements is larger than allowed
+            int gap = Math.Abs(_data[i+1] - _data[i]);
+            if(gap < 1 || gap > 3)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+
+    
+    
+}
