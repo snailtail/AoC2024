41d501f0de24e2e06ddeaac5c7642da47ddd7539
Magnus Olofsson <magnusolofsson@outlook.com>
2024-12-02 07:10:32 +0100

    Working on day 2

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var unusualData = File.ReadLines("../data/02test.dat").ToArray().Select(l => l.Split(' ').Select(i => int.Parse(i)).ToArray()).ToArray();
+
+
+
+foreach(var entry in unusualData)
+{
+    //check "direction"
+    bool isIncreasing = false;
+    isIncreasing = entry[1] > entry[0];
+    var result = isValidStep1(entry,isIncreasing);
+}
+
+
+
+bool isValidStep1(int[] arr, bool isIncreasing)
+{
+    
+    for(int i = 0; i < arr.Length-1; i++)
+    {
+        if((isIncreasing && arr[i+1] < arr[i]) || (!isIncreasing && arr[i+1] > arr[i]))
+        {
+            // 
+            return false;
+        }
+    }
+}
+Console.WriteLine("");
+
