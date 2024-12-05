c506e1ba332504f923baf84992a60c593a80ecb6
Magnus Olofsson <magnusolofsson@outlook.com>
2024-12-05 06:49:47 +0100

    Started on Part 2

--- 05/Program.cs
+++ 05/Program.cs
@@ -4,15 +4,21 @@ var path = "05.dat";
 var rules = File.ReadAllLines(path).Where(l => l.Contains("|")).ToList();
 var updates = File.ReadAllLines(path).Where(l => l.Contains(",")).ToList();
 int resultPart1 = 0;
+int resultPart2 = 0;
 foreach (var update in updates)
 {
     if (isValidUpdate(update, rules))
     {
         resultPart1 += getMiddleItem(update);
     }
+    else
+    {
+        resultPart2 += getFixedUpdateMiddleItem(update);
+    }
 }
 
 Console.WriteLine($"Part 1: {resultPart1}");
+Console.WriteLine($"Part 2: {resultPart2}");
 
 bool isValidUpdate(string update, List<string> rules)
 {
@@ -24,7 +30,6 @@ bool isValidUpdate(string update, List<string> rules)
         if (rule1Index != -1 && rule2Index != -1 && rule1Index > rule2Index)
         {
             return false;
-            Console.WriteLine(update);
         }
     }
     return true;
@@ -35,4 +40,9 @@ int getMiddleItem(string update)
     var parts = update.Split(",");
     int middleItem = parts.Length / 2;
     return int.Parse(parts[middleItem]);
+}
+
+int getFixedUpdateMiddleItem(string s)
+{
+    throw new NotImplementedException();
 }
