0ce5f66bf8ce06f30897ae047d614e45a272a5ff
Magnus Olofsson <magnusolofsson@outlook.com>
2024-12-07 09:33:36 +0100

    Solved day 7

--- 07/Program.cs
+++ 07/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _07.Model;
+var input = File.ReadAllLines("07.dat");
+var resultPart1 = BridgeCalibrationDescramblerService.Part1(input);
+Console.WriteLine($"Part 1 : {resultPart1}");
+
+var resultPart2 = BridgeCalibrationDescramblerService.Part2(input);
+Console.WriteLine($"Part 2 : {resultPart2}");
--- tests/07-test/UnitTest1.cs
+++ tests/07-test/UnitTest1.cs
@@ -1,10 +1,74 @@
+using _07.Model;
+using Shouldly;
+
 namespace _07_test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("190: 10 19",true)]
+    [InlineData("3267: 81 40 27", true)]
+    [InlineData("83: 17 5", false)]
+    [InlineData("292: 11 6 16 20", true)]
+    [InlineData("156: 15 6", false)]
+    [InlineData("7290: 6 8 6 15", false)]
+    [InlineData("161011: 16 10 13", false)]
+    [InlineData("192: 17 8 14", false)]
+    [InlineData("21037: 9 7 18 13", false)]
+    public void Part1_Check_IsValidCalibration(string input, bool expectedStatus)
     {
+        (long testValue, List<long> numbers) = BridgeCalibrationDescramblerService.ParseEquation(input);
+        bool isValid = BridgeCalibrationDescramblerService.IsValidCalibration(testValue, numbers.ToArray(),false);
+        isValid.ShouldBe(expectedStatus);
+    }
 
+    [Fact]
+    public void Part1_Check_Final_Result_Is_3749()
+    {
+        string[] testinput = @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20".Split("\r\n");
+        var result = BridgeCalibrationDescramblerService.Part1(testinput);
+        result.ShouldBe(3749);
+    }
+    
+    
+    [Theory]
+    [InlineData("190: 10 19",true)]
+    [InlineData("3267: 81 40 27", true)]
+    [InlineData("83: 17 5", false)]
+    [InlineData("292: 11 6 16 20", true)]
+    [InlineData("156: 15 6", true)]
+    [InlineData("7290: 6 8 6 15", true)]
+    [InlineData("161011: 16 10 13", false)]
+    [InlineData("192: 17 8 14", true)]
+    [InlineData("21037: 9 7 18 13", false)]
+    public void Part2_Check_IsValidCalibration(string input, bool expectedStatus)
+    {
+        (long testValue, List<long> numbers) = BridgeCalibrationDescramblerService.ParseEquation(input);
+        bool isValid = BridgeCalibrationDescramblerService.IsValidCalibration(testValue, numbers.ToArray(),true);
+        isValid.ShouldBe(expectedStatus);
+    }
+    
+    [Fact]
+    public void Part2_Check_Final_Result_Is_11387()
+    {
+        string[] testinput = @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20".Split("\r\n");
+        var result = BridgeCalibrationDescramblerService.Part2(testinput);
+        result.ShouldBe(11387);
     }
 }
