038e2680e84af6bbc9581bb5e90b2a29a9546eda
Magnus Olofsson <magnusolofsson@outlook.com>
2024-12-09 16:37:11 +0100

    Add day 10 skeleton

---
+++ tests/10-test/UnitTest1.cs
@@ -0,0 +1,24 @@
+namespace _10_test;
+
+public class Day10
+{
+    
+}
+
+public class Day10Test
+{
+    
+}
+
+public class PartsTest
+{
+    [Fact(Skip="Not implemented yet")]
+    public void Part1()
+    {
+    }
+    
+    [Fact(Skip="Not implemented yet")]
+    public void Part2()
+    {
+    }
+}
