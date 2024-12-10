bc5023d36c1302e002b5ce8ec1b55cba24009b74
Magnus Olofsson <magnusolofsson@outlook.com>
2024-12-10 19:31:35 +0100

    Moving to using only Unit test projects

--- 06/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿var mapdata = File.ReadAllLines("06.dat").Select(line => line.ToCharArray()).ToArray();
-
-var resultPart1 = SuitManufacturingLabMapHandler.walkRegularPath(mapdata).Count;
-Console.WriteLine($"Part 1: {resultPart1}");
-
-var resultPart2 = SuitManufacturingLabMapHandler.GetCombinationsOfPossibleLoops(mapdata);
-Console.WriteLine($"Part 2: {resultPart2}");
--- 07/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using _07.Model;
-var input = File.ReadAllLines("07.dat");
-var resultPart1 = BridgeCalibrationDescramblerService.Part1(input);
-Console.WriteLine($"Part 1 : {resultPart1}");
-
-var resultPart2 = BridgeCalibrationDescramblerService.Part2(input);
-Console.WriteLine($"Part 2 : {resultPart2}");
