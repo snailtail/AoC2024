6f69ea204038423da24a856be21424c76a9b3134
Magnus Olofsson <magnusolofsson@outlook.com>
2024-12-10 20:47:35 +0100

    Moving to Unit test only

--- tests/02-test/Day02Tests.cs
+++ tests/02-test/Day02Tests.cs
@@ -1,4 +1,64 @@
 namespace _02_test;
+
+public class unusualData
+{
+    private int[] _data;
+    public int[] Data
+    {
+        get { return _data; }
+        set { _data = value; }
+    }
+
+    public unusualData(int[] data)
+    {
+        _data = data;
+    }
+
+    
+
+    public bool IsValid => isValid(_data,false);
+    public bool IsAlmostValid => isValid(_data,true);
+
+    private bool isValid(int[] arr, bool allowErrors = false)
+    {    
+        bool isIncreasing = arr[1] > arr[0];
+        for(int i = 0; i < arr.Length-1; i++)
+        {
+
+            // Check if increasing or decreasing breaks
+            if(
+                (isIncreasing && arr[i+1] < arr[i]) 
+                || 
+                (!isIncreasing && arr[i+1] > arr[i]) 
+                || 
+                arr[i+1] == arr[i]
+                ||
+                (Math.Abs(arr[i+1] - arr[i]) < 1 || Math.Abs(arr[i+1] - arr[i]) > 3)
+            )
+            {
+                // so we have an error
+                // for step 2 - we can check if this array would be valid if we remove any value
+                if(allowErrors){
+                    for(int ii = 0; ii < arr.Length; ii++)
+                    {
+                        int[] newArr = arr.Where((_, index) => index != ii).ToArray();
+                        bool isAlmostValid = isValid(newArr,false);
+                        if(isAlmostValid)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                return false;
+            }
+
+            
+        }
+
+        return true;
+    }    
+}
 public class Day02Tests
 {
     [Theory]
@@ -79,5 +139,30 @@ public class Day02Tests
         // Validate
         Assert.True(result);
         
+    }
+
+    [Fact]
+    public void Part1()
+    {
+        unusualData[] uData = File.ReadLines("02test.dat").ToArray().Select(l => l.Split(' ').Select(i => int.Parse(i)).ToArray()).Select(arr => new unusualData(arr)).ToArray();
+        int step1ValidCount = 0;
+        foreach(var entry in uData)
+        {    
+            step1ValidCount += entry.IsValid ? 1 : 0;
+        }
+        Assert.Equal(2,step1ValidCount);
+    }
+    
+    [Fact]
+    public void Part2()
+    {
+        unusualData[] uData = File.ReadLines("02test.dat").ToArray().Select(l => l.Split(' ').Select(i => int.Parse(i)).ToArray()).Select(arr => new unusualData(arr)).ToArray();
+        
+        int step2ValidCount = 0;
+        foreach(var entry in uData)
+        {    
+            step2ValidCount += entry.IsAlmostValid ? 1 : 0;
+        }
+        Assert.Equal(4,step2ValidCount);
     }
 }
